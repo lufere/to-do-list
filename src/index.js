ce3e55c7564cc64119da199b264a9a2d124ef5f2
Luis Rendon <luisfelipe.rendon@gmail.com>
2020-05-05 23:54:14 -0700

    Added the list module with functions to set the current project, add, edit and categorize todos

--- src/index.js
+++ src/index.js
@@ -8,10 +8,72 @@ const Project = (title, color) =>{
     const add = (todo)=>{
         todoList.push(todo);
     }
-    return {title, color, todoList, add}
+    const remove = (position) =>{
+        todoList.splice(position,1);
+    }
+
+    const findPosition = (title) =>{
+        return todoList.findIndex(element => element.title == title);
+
+    }
+
+    return {title, color, todoList, add, remove, findPosition}
 }
 
+const list = (()=>{
+    let currentProject;
+    const setProject = (project) =>{
+        currentProject = project;
+        console.log(currentProject);
+    }
+
+    const createTodo = (title, description, dueDate, priority) =>{
+        var test = Todo(title, description, dueDate, priority)
+        currentProject.add(test);
+    }
+    
+    const removeTodo = (position) =>{
+        currentProject.remove(position);
+    }
+
+    const toggleCompletion = (title) =>{
+        let position = currentProject.findPosition(title);
+        currentProject.todoList[position].completed = !currentProject.todoList[position].completed;
+    }
+
+    const togglePriority = (title)=>{
+        let position = currentProject.findPosition(title);
+        currentProject.todoList[position].priority = !currentProject.todoList[position].priority
+    }
+
+    const changeDescription = (title, description) =>{
+        let position = currentProject.findPosition(title);
+        currentProject.todoList[position].description = description;
+    }
+
+    const changeDueDate = (title, dueDate) =>{
+        let position = currentProject.findPosition(title);
+        currentProject.todoList[position].dueDate = dueDate;
+    }
+
+    return{setProject, createTodo,removeTodo,toggleCompletion, changeDescription, changeDueDate, togglePriority}
+
+})()
+
 const initial = Project("default", "yellow");
-const test = Todo ("Walk the dog", "Take the dog for a walk", "tomorrow", 1, initial);
-initial.add(test);
+list.setProject(initial);
+list.createTodo("Walk the dog", "Take the dog for a walk", "tomorrow", true);
+list.createTodo("Walk the dog2", "Take the dog for a walk", "tomorrow", false);
 console.table(initial.todoList);
+list.togglePriority("Walk the dog");
+list.changeDescription("Walk the dog", "Don't walk him anymore he's a bad boy");
+list.changeDueDate("Walk the dog", "NEVER")
+console.table(initial.todoList);
+
+function test(e){
+    classTest = this.querySelector(".title");
+    //console.log (classTest.innerHTML);
+    console.log(initial.findPosition(classTest.innerHTML));
+
+}
+document.querySelector("#one").addEventListener("click",test);
